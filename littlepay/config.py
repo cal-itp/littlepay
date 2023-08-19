07fb19d8379f8821b452a6a4a86a8f1a70aaac80
Kegan Maher <kegan@compiler.la>
2023-08-19 16:55:50 +0000

    feat(config): parse config file at path

--- littlepay/config.py
+++ littlepay/config.py
@@ -1,10 +1,28 @@
 import os
 from pathlib import Path
+import yaml
 
 
 CONFIG_DIR = Path(os.environ.get("LP_CONFIG_DIR", "~/.littlepay")).expanduser()
 CONFIG_FILE_CURRENT = CONFIG_DIR / ".current"
 DEFAULT_CONFIG_FILE = CONFIG_DIR / "config.yaml"
+
+ENV_QA = "qa"
+ENV_PROD = "prod"
+ENVS = [ENV_QA, ENV_PROD]
+
+CONFIG_ACTIVE = "active"
+CONFIG_ENV = "env"
+CONFIG_ENVS = f"{CONFIG_ENV}s"
+CONFIG_PARTICIPANT = "participant"
+CONFIG_PARTICIPANTS = f"{CONFIG_PARTICIPANT}s"
+CONFIG_TYPES = [CONFIG_ENV, CONFIG_PARTICIPANT]
+
+DEFAULT_CONFIG = {
+    CONFIG_ACTIVE: {CONFIG_ENV: ENV_QA, CONFIG_PARTICIPANT: ""},
+    f"{CONFIG_ENVS}": {ENV_QA: {"url": ""}, ENV_PROD: {"url": ""}},
+    f"{CONFIG_PARTICIPANTS}": {"cst": {"client_id": "", "client_secret": "", "audience": ""}},
+}
 
 
 def get_config_path() -> Path:
@@ -13,3 +31,33 @@ def get_config_path() -> Path:
     CONFIG_FILE_CURRENT.touch()
     current = CONFIG_FILE_CURRENT.read_text().strip()
     return Path(current or DEFAULT_CONFIG_FILE)
+
+
+def _read_config(config_file_path: Path = None) -> dict:
+    if config_file_path is None:
+        config_file_path = get_config_path()
+    return yaml.safe_load(config_file_path.read_text())
+
+
+def _write_config(config: dict, config_file_path: Path = None) -> None:
+    if config_file_path is None:
+        config_file_path = get_config_path()
+    config_file_path.write_text(yaml.dump(config))
+
+
+def get_config(config_file_path: str | Path = None, reset: bool = False) -> dict:
+    """If config_file_path does not exist, creates it with a default configuration.
+
+    Returns a dict representation of the config file.
+    """
+    if config_file_path is None or config_file_path == "":
+        config_file_path = get_config_path()
+    if isinstance(config_file_path, str):
+        config_file_path = Path(config_file_path)
+    if not config_file_path.exists() or reset:
+        print(f"Creating config file: {config_file_path.resolve()}")
+        config_file_path.parent.mkdir(parents=True, exist_ok=True)
+        _write_config(DEFAULT_CONFIG, config_file_path)
+
+    CONFIG_FILE_CURRENT.write_text(str(config_file_path.resolve()))
+    return _read_config(config_file_path)
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,27 @@
 from pathlib import Path
 
-from littlepay.config import get_config_path
+import pytest
+
+import littlepay.config
+from littlepay.config import DEFAULT_CONFIG, get_config_path, get_config
+
+
+CUSTOM_CONFIG_FILE = "./tests/test.config.yaml"
+
+
+@pytest.fixture(autouse=True)
+def custom_config_file():
+    """Fixture overrides littlepay.config.DEFAULT_CONFIG_FILE for the duration of a test, resetting it back at the end."""
+    default = littlepay.config.DEFAULT_CONFIG_FILE
+
+    custom = Path(CUSTOM_CONFIG_FILE)
+    custom.unlink(missing_ok=True)
+    littlepay.config.DEFAULT_CONFIG_FILE = custom
+
+    yield littlepay.config.DEFAULT_CONFIG_FILE
+
+    custom.unlink(missing_ok=True)
+    littlepay.config.DEFAULT_CONFIG_FILE = default
 
 
 def test_get_config_path_default(custom_config_file: Path):
@@ -17,3 +38,39 @@ def test_get_config_path_current(custom_current_file: Path):
     result = get_config_path()
 
     assert result == Path(expected)
+
+
+@pytest.mark.parametrize("path_arg", [None, "", CUSTOM_CONFIG_FILE, Path(CUSTOM_CONFIG_FILE)])
+def test_get_config(path_arg, custom_current_file: Path, custom_config_file: Path):
+    assert not custom_current_file.exists()
+    assert not custom_config_file.exists()
+
+    config = get_config(path_arg)
+
+    assert custom_current_file.exists()
+    current_config_path = Path(custom_current_file.read_text())
+
+    assert current_config_path.resolve() == custom_config_file.resolve()
+    assert custom_config_file.exists()
+    assert config == DEFAULT_CONFIG
+
+
+def test_get_config_exists(custom_config_file: Path):
+    assert not custom_config_file.exists()
+    custom_config_file.write_text("the config")
+    assert custom_config_file.exists()
+
+    config = get_config(custom_config_file)
+
+    assert config == "the config"
+
+
+def test_get_config_reset(custom_config_file: Path):
+    assert not custom_config_file.exists()
+    custom_config_file.write_text("the config")
+    assert custom_config_file.exists()
+
+    config = get_config(custom_config_file, reset=True)
+
+    assert custom_config_file.exists()
+    assert config == DEFAULT_CONFIG
