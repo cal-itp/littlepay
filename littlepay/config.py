ed4aed700c1731abb2ecd2830be4b07375534479
Kegan Maher <kegan@compiler.la>
2023-08-19 16:55:50 +0000

    feat(config): read and write config file at path

--- littlepay/config.py
+++ littlepay/config.py
@@ -1,14 +1,32 @@
 import os
 from pathlib import Path
+import yaml
 
 
 CONFIG_DIR = Path(os.environ.get("LP_CONFIG_DIR", "~/.littlepay")).expanduser()
 CONFIG_FILE_CURRENT = CONFIG_DIR / ".current"
 DEFAULT_CONFIG_FILE = CONFIG_DIR / "config.yaml"
 
+ENV_QA = "qa"
+ENV_PROD = "prod"
+ENVS = [ENV_QA, ENV_PROD]
 
-def get_config_path() -> Path:
-    """Gets a pathlib.Path of the config file currently in-use, or the default if None."""
+DEFAULT_ACTIVE = {"env": ENV_QA, "participant": ""}
+DEFAULT_CREDENTIALS = {"client_id": "", "client_secret": "", "audience": ""}
+DEFAULT_ENV = {"url": "", "version": "v1"}
+DEFAULT_CONFIG = {
+    "active": DEFAULT_ACTIVE,
+    "envs": {ENV_QA: DEFAULT_ENV, ENV_PROD: DEFAULT_ENV},
+    "participants": {"cst": {ENV_QA: DEFAULT_CREDENTIALS, ENV_PROD: DEFAULT_CREDENTIALS}},
+}
+CONFIG_TYPES = list(DEFAULT_ACTIVE.keys())
+
+
+def _get_current_path() -> Path:
+    """
+    Returns (Path):
+        The path to the config file currently in-use, or the default.
+    """
     CONFIG_FILE_CURRENT.parent.mkdir(parents=True, exist_ok=True)
     if not CONFIG_FILE_CURRENT.exists():
         CONFIG_FILE_CURRENT.touch()
@@ -22,3 +40,123 @@ def _update_current_path(new_path: str | Path):
     if isinstance(new_path, Path):
         new_path = str(new_path.expanduser().absolute())
     CONFIG_FILE_CURRENT.write_text(new_path)
+
+
+def _update_current_path(new_path: str | Path):
+    """Saves new_path as the path to the current config file."""
+    if isinstance(new_path, Path):
+        new_path = str(new_path.expanduser().absolute())
+    CONFIG_FILE_CURRENT.write_text(new_path)
+
+
+def _read_config(config_file: Path) -> dict:
+    """Reads configuration data from config_file."""
+    return yaml.safe_load(config_file.read_text())
+
+
+def _write_config(config: dict, config_file: Path) -> None:
+    """Writes configuration data to config_file."""
+    class NoAliasDumper(yaml.SafeDumper):
+        """Forces pyyaml to write without aliases.
+
+        See https://github.com/yaml/pyyaml/issues/103.
+        """
+        def ignore_aliases(self, _):
+            return True
+
+    config_file.write_text(yaml.dump(config, Dumper=NoAliasDumper))
+
+class Config:
+    """Interface to the configuration backend."""
+
+    current_path = staticmethod(_get_current_path)
+    update_path = staticmethod(_update_current_path)
+    read = staticmethod(_read_config)
+    write = staticmethod(_write_config)
+
+    def __init__(self, config_file_path: str | Path = None, reset: bool = False):
+        """Initialize a new Config instance, reading from the given path or a default location.
+
+        Args:
+            config_file_path (str|Path): Path to a readable config file. If None, the default is used.
+
+            reset (bool): True to reset the configuration to the default.
+        """
+        if config_file_path is None or config_file_path == "":
+            config_file_path = Config.current_path()
+        if isinstance(config_file_path, str):
+            config_file_path = Path(config_file_path)
+        if not config_file_path.exists() or reset:
+            print(f"Creating config file: {config_file_path.resolve()}")
+            config_file_path.parent.mkdir(parents=True, exist_ok=True)
+            Config.write(DEFAULT_CONFIG, config_file_path)
+
+        Config.update_path(config_file_path)
+
+        data = Config.read(config_file_path)
+        for key, value in data.items():
+            setattr(self, key, value)
+
+    def active_credentials(self, required: bool = False) -> dict:
+        """Get credentials from the active participant's environment config.
+
+        Args:
+            required (bool): If True, raise a ValueError for missing credentials.
+        """
+        participant = self.active_participant()
+        for key in [k for k in DEFAULT_CREDENTIALS.keys() if required and k not in participant]:
+            raise ValueError(f"Participant missing required credential: {key}")
+        return {key: value for key, value in participant.items() if key in DEFAULT_CREDENTIALS.keys()}
+
+    def active_env(self) -> dict:
+        """
+        Returns (dict):
+            Configuration data for the active environment.
+        """
+        return self.envs[self.active_env_name()]
+
+    def active_env_name(self, new_env: str = None) -> str:
+        """Get or set the active environment by name. By default, the QA environment.
+
+        Args:
+            new_env (str): The new environment name to activate. Must exist.
+
+        Returns (str):
+            The name of the active environment.
+        """
+        if new_env is not None:
+            if new_env not in self.envs:
+                raise ValueError(f"Unsupported env: {new_env}, must be one of: {', '.join(self.envs.keys())}")
+
+            self.active["env"] = new_env
+            Config.write(self.__dict__, Config.current_path())
+
+        return self.active.get("env", ENV_QA)
+
+    def active_participant(self) -> dict:
+        """
+        Returns (dict):
+            Configuration data for the active participant.
+        """
+        return self.participants[self.active_participant_id()][self.active_env_name()]
+
+    def active_participant_id(self, new_participant: str = None) -> str:
+        """Get or set the active participant by participant_id.
+
+        Args:
+            new_participant (str): The new participant_id to activate. Must exist.
+
+        Returns (str):
+            The participant_id of the active participant.
+        """
+        if new_participant is not None:
+            if new_participant not in self.participants:
+                raise ValueError(
+                    f"Unsupported participant: {new_participant}, must be one of: {', '.join(self.participants.keys())}"
+                )
+
+            self.active["participant"] = new_participant
+            Config.write(self.__dict__, Config.current_path())
+
+        # ensure active is always a str, even if missing (e.g. None)
+        return self.active.get("participant", "") or ""
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,19 +1,262 @@
 from pathlib import Path
 
-from littlepay.config import get_config_path
+import pytest
+
+import littlepay.config
+from littlepay.config import (
+    DEFAULT_CONFIG,
+    DEFAULT_CREDENTIALS,
+    ENV_QA,
+    _get_current_path,
+    _read_config,
+    _write_config,
+    _update_current_path,
+    Config,
+)
+from tests.conftest import CUSTOM_CONFIG_FILE
 
 
-def test_get_config_path_default(custom_config_file: Path):
-    result = get_config_path()
+def test_get_current_path_default(custom_config_file: Path):
+    result = _get_current_path()
 
     assert isinstance(result, Path)
     assert result.absolute() == custom_config_file.absolute()
 
 
-def test_get_config_path_current(custom_current_file: Path):
+def test_get_current_path_custom(custom_current_file: Path):
     expected = "."
     custom_current_file.write_text(expected)
 
-    result = get_config_path()
+    result = _get_current_path()
 
     assert result == Path(expected)
+
+
+def test_get_current_path_newline(custom_current_file: Path):
+    expected = "."
+    custom_current_file.write_text(".\n")
+
+    result = _get_current_path()
+
+    assert result == Path(expected)
+
+
+def test_update_current_path_str(custom_current_file: Path):
+    assert not custom_current_file.exists()
+
+    _update_current_path("/the/path")
+
+    assert custom_current_file.read_text() == "/the/path"
+
+
+def test_update_current_path_Path(custom_current_file: Path):
+    assert not custom_current_file.exists()
+
+    _update_current_path(Path("/the/path"))
+
+    assert custom_current_file.read_text() == "/the/path"
+
+
+def test_read_config(custom_config_file: Path):
+    custom_config_file.write_text("config: the config")
+
+    config = _read_config(custom_config_file)
+
+    assert config == {"config": "the config"}
+
+
+def test_write_config(custom_config_file: Path):
+    assert not custom_config_file.exists()
+
+    _write_config({"config": "the config"}, custom_config_file)
+
+    text = custom_config_file.read_text().strip()
+    assert text.startswith("config:")
+    assert text.endswith("the config")
+
+
+@pytest.mark.parametrize("path_arg", [None, "", CUSTOM_CONFIG_FILE, Path(CUSTOM_CONFIG_FILE)])
+def test_Config(path_arg, custom_current_file: Path, custom_config_file: Path):
+    assert not custom_current_file.exists()
+    assert not custom_config_file.exists()
+
+    config = Config(path_arg)
+
+    assert custom_current_file.exists()
+    current_config_path = Path(custom_current_file.read_text())
+
+    assert current_config_path.resolve() == custom_config_file.resolve()
+    assert custom_config_file.exists()
+    assert config.active == DEFAULT_CONFIG["active"]
+    assert config.envs == DEFAULT_CONFIG["envs"]
+    assert config.participants == DEFAULT_CONFIG["participants"]
+    assert not hasattr(config, "token")
+
+
+def test_Config_exists(custom_config_file: Path):
+    assert not custom_config_file.exists()
+    custom_config_file.write_text('{"data": "the config", "other_data": "more config"}')
+    assert custom_config_file.exists()
+
+    config = Config(custom_config_file)
+
+    assert config.data == "the config"
+    assert config.other_data == "more config"
+
+
+def test_Config_reset(custom_config_file: Path):
+    assert not custom_config_file.exists()
+    custom_config_file.write_text('{"data": "the config"}')
+    assert custom_config_file.exists()
+
+    config = Config(custom_config_file, reset=True)
+
+    assert custom_config_file.exists()
+    assert config.active == DEFAULT_CONFIG["active"]
+    assert config.envs == DEFAULT_CONFIG["envs"]
+    assert config.participants == DEFAULT_CONFIG["participants"]
+    assert not hasattr(config, "data")
+
+
+def test_Config_active_env_name(mocker):
+    config = Config()
+    config.active = mocker.Mock()
+    config.active.get = mocker.Mock(return_value="active_env")
+
+    assert config.active_env_name() == "active_env"
+
+
+def test_Config_active_env_name_default():
+    config = Config()
+    config.active = {}
+
+    assert config.active_env_name() == ENV_QA
+
+
+def test_Config_active_env_name_update():
+    config = Config()
+    config.active = {"env": "original_env"}
+    config.envs = {"original_env": "the original env", "new_env": "the new env"}
+
+    result = config.active_env_name("new_env")
+
+    assert result == "new_env"
+    assert config.active_env_name() == result
+
+
+def test_Config_active_env_name_update_unsupported(mocker):
+    spy_write = mocker.spy(littlepay.config, "_write_config")
+    config = Config()
+    config.active = {"env": "original_env"}
+    config.envs = {"original_env": "the original env"}
+
+    with pytest.raises(ValueError):
+        config.active_env_name("new_env")
+
+    assert spy_write.call_count == 0
+    assert config.active_env_name() == "original_env"
+
+
+def test_Config_active_env():
+    config = Config()
+    config.active["env"] = "env123"
+    config.envs["env123"] = "the environment"
+
+    assert config.active_env() == "the environment"
+
+
+def test_Config_active_participant_id(mocker):
+    config = Config()
+    config.active = mocker.Mock()
+    config.active.get = mocker.Mock(return_value="active_participant")
+
+    assert config.active_participant_id() == "active_participant"
+
+
+def test_Config_active_participant_id_default():
+    config = Config()
+    config.active = {}
+
+    assert config.active_participant_id() == ""
+
+
+def test_Config_active_participant_id_update():
+    config = Config()
+    config.active = {"participant": "participant123"}
+    config.participants = {"participant123": "one two three", "participant456": "four five six"}
+
+    result = config.active_participant_id("participant456")
+
+    assert result == "participant456"
+    assert config.active_participant_id() == result
+
+
+def test_Config_active_participant_id_update_unsupported(mocker):
+    spy_write = mocker.spy(littlepay.config, "_write_config")
+    config = Config()
+    config.active = {"participant": "participant123"}
+    config.participants = {"participant123": "one two three"}
+
+    with pytest.raises(ValueError):
+        config.active_participant_id("participant456")
+
+    assert spy_write.call_count == 0
+    assert config.active_participant_id() == "participant123"
+
+
+def test_Config_active_participant():
+    config = Config()
+    config.active["env"] = "env123"
+    config.active["participant"] = "participant123"
+    config.participants = {"participant123": {"env123": "the participant config"}}
+
+    assert config.active_participant() == "the participant config"
+
+
+def test_Config_active_credentials_default(mocker):
+    config = Config()
+    mocker.patch.object(config, "active_participant", return_value={})
+
+    credentials = config.active_credentials()
+
+    assert credentials == {}
+
+
+def test_Config_active_credentials_custom(mocker):
+    custom = dict(**DEFAULT_CREDENTIALS)
+    custom["client_id"] = "the ID"
+    custom["client_secret"] = "123456"
+    custom["audience"] = "the audience"
+
+    config = Config()
+    mocker.patch.object(config, "active_participant", return_value=custom)
+
+    credentials = config.active_credentials()
+
+    assert credentials == custom
+
+
+def test_Config_active_credentials_missing(mocker):
+    config = Config()
+    mocker.patch.object(config, "active_participant", return_value={"env": "something", "data": "other"})
+
+    credentials = config.active_credentials()
+
+    assert credentials == {}
+
+
+def test_Config_active_credentials_required(mocker):
+    config = Config()
+    mocker.patch.object(config, "active_participant", return_value=DEFAULT_CREDENTIALS)
+
+    credentials = config.active_credentials(required=True)
+
+    assert credentials == DEFAULT_CREDENTIALS
+
+
+def test_Config_active_credentials_required_missing(mocker):
+    config = Config()
+    mocker.patch.object(config, "active_participant", return_value={})
+
+    with pytest.raises(ValueError):
+        config.active_credentials(required=True)
