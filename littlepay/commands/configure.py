f4d4586a407f95feeaa30cf02d535467247a1414
Kegan Maher <kegan@compiler.la>
2023-08-22 06:11:57 +0000

    feat(config): result indicates if env, participant initialized
    participant should have all auth keys for the env

--- littlepay/commands/configure.py
+++ littlepay/commands/configure.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from littlepay.commands import RESULT_SUCCESS
+from littlepay.commands import RESULT_FAILURE, RESULT_SUCCESS
 from littlepay.config import active_env, active_participant, all_envs, all_participants, get_config, get_config_path
 
 
@@ -8,7 +8,7 @@ def configure(config_path: str | Path = None, reset: bool = False) -> int:
     """Get or set project configuration.
 
     Returns:
-        A value indicating if the operation succeeded or failed.
+        A value indicating whether or not the current environment and participant are configured.
     """
     if config_path is None:
         config_path = get_config_path()
@@ -16,15 +16,26 @@ def configure(config_path: str | Path = None, reset: bool = False) -> int:
     current_config_path = get_config_path()
 
     env, _ = active_env(config)
-    participant, _ = active_participant(config)
+    participant, participant_config = active_participant(config)
 
     print(f"Config: {current_config_path}")
     print(f"Envs: {', '.join(all_envs().keys())}")
     print(f"Participants: {', '.join(all_participants().keys())}")
 
-    if participant != "":
-        print(f"Active: {env}, {participant}")
-    else:
-        print(f"Active: {env} [no participant]")
+    if participant == "":
+        print(f"Active: {env}, [no participant]")
+        return RESULT_FAILURE
 
-    return RESULT_SUCCESS
+    participant_env = participant_config.get(env, {})
+    participant_auth = (
+        participant_env.get("client_id"),
+        participant_env.get("client_secret"),
+        participant_env.get("audience"),
+    )
+
+    if not all(participant_auth):
+        print(f"Active: {env}, {participant} [missing auth]")
+        return RESULT_FAILURE
+    else:
+        print(f"Active: {env}, {participant}")
+        return RESULT_SUCCESS
--- tests/commands/test_configure.py
+++ tests/commands/test_configure.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 
-from littlepay.commands import RESULT_SUCCESS
+from littlepay.commands import RESULT_FAILURE, RESULT_SUCCESS
 from littlepay.commands.configure import configure
 
 
-def test_configure(custom_config_file: Path, capfd):
+def test_configure(mocker, custom_config_file, capfd):
     assert not custom_config_file.exists()
+    mocker.patch("littlepay.commands.configure.get_config", return_value={})
 
     res = configure(custom_config_file)
     capture = capfd.readouterr()
 
-    assert res == RESULT_SUCCESS
+    assert res == RESULT_FAILURE
     assert custom_config_file.exists()
     assert "Config:" in capture.out
     assert "Envs:" in capture.out
@@ -19,32 +20,50 @@ def test_configure(custom_config_file: Path, capfd):
     assert "[no participant]" in capture.out
 
 
-def test_configure_default(custom_config_file: Path, capfd):
-    assert not custom_config_file.exists()
+def test_configure_default(mocker, capfd):
+    mocker.patch("littlepay.commands.configure.active_env", return_value=("env123", {}))
 
     res = configure()
     capture = capfd.readouterr()
 
-    assert res == RESULT_SUCCESS
-    assert custom_config_file.exists()
+    assert res == RESULT_FAILURE
     assert "Config:" in capture.out
     assert "Envs:" in capture.out
     assert "Participants:" in capture.out
-    assert "Active:" in capture.out
-    assert "[no participant]" in capture.out
+    assert "Active: env123, [no participant]" in capture.out
 
 
-def test_configure_participant(mocker, capfd):
-    mocker.patch("littlepay.commands.configure.active_participant", return_value=("participant123", {}))
+def test_configure_participant_auth(mocker, capfd):
+    mocker.patch("littlepay.commands.configure.active_env", return_value=("env123", {}))
+    mocker.patch(
+        "littlepay.commands.configure.active_participant",
+        return_value=(
+            "participant123",
+            {"env123": {"client_id": "client id", "client_secret": "client secret", "audience": "audience"}},
+        ),
+    )
     res = configure()
     capture = capfd.readouterr()
 
-    assert res == RESULT_SUCCESS
+    assert res == RESULT_FAILURE
     assert "Config:" in capture.out
     assert "Envs:" in capture.out
     assert "Participants:" in capture.out
-    assert "Active:" in capture.out
-    assert "participant123" in capture.out
+    assert "Active: env123, participant123" in capture.out
+    assert "[missing auth]" not in capture.out
+
+
+def test_configure_participant_no_auth(mocker, capfd):
+    mocker.patch("littlepay.commands.configure.active_env", return_value=("env123", {}))
+    mocker.patch("littlepay.commands.configure.active_participant", return_value=("participant123", {"env123": {}}))
+    res = configure()
+    capture = capfd.readouterr()
+
+    assert res == RESULT_FAILURE
+    assert "Config:" in capture.out
+    assert "Envs:" in capture.out
+    assert "Participants:" in capture.out
+    assert "Active: env123, participant123 [missing auth]" in capture.out
 
 
 def test_configure_reset(custom_config_file: Path):
@@ -55,6 +74,6 @@ def test_configure_reset(custom_config_file: Path):
 
     res = configure(custom_config_file, reset=True)
 
-    assert res == RESULT_SUCCESS
+    assert res == RESULT_FAILURE
     assert custom_config_file.exists()
     assert content not in custom_config_file.read_text()
