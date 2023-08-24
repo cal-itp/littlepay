cb3ceef75749f28c4cbcd8e3aeb9083f50ded161
Kegan Maher <kegan@compiler.la>
2023-08-24 07:08:02 +0000

    refactor(api): use requests directly to send json body
    authlib sends a form-encoded body in POST with no way to override
    
    Littlepay requires a JSON-encoded body, implement token refresh by hand

--- littlepay/api/client.py
+++ littlepay/api/client.py
@@ -1,29 +1,84 @@
-from authlib.integrations.requests_client import OAuth2Session
+import time
+
+from requests import Request, Session
+
+from littlepay import __version__ as version
+
+
+def _current_time() -> int:
+    """Returns the current time as integer seconds since Unix epoch."""
+    return int(time.time())
 
 
 class Client:
     """Represents an API connection to an environment."""
 
-    def __init__(self, env_api_url: str, client_id: str, client_secret: str, audience: str, token: str = None):
-        """Initialize a new Client to connect to an API environment. Optionally use an existing access token."""
-        self.env_api_url = env_api_url
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.audience = audience
-        self.session = OAuth2Session(
-            token_endpoint=self.token_endpoint,
-            client_id=self.client_id,
-            client_secret=self.client_secret,
-            audience=self.audience,
+    current_time = staticmethod(_current_time)
+
+    def __init__(
+        self,
+        env_api_url: str,
+        client_id: str,
+        client_secret: str,
+        audience: str,
+        token: dict = None,
+        env_api_version: str = "v1",
+    ):
+        """Initialize a new Client for an API environment. Optionally use an existing access token."""
+        self.credentials = dict(
+            audience=audience,
+            client_id=client_id,
+            client_secret=client_secret,
             grant_type="client_credentials",
-            token=token,
         )
+        self.env_api_url = env_api_url
+        self.env_api_version = env_api_version
+
+        self.session = Session()
+        self.session.headers.update(
+            {"Accept": "application/json", "Content-Type": "application/json", "User-Agent": f"cal-itp/littlepay:{version}"}
+        )
+
+        if isinstance(token, dict):
+            self._token = token
+            self._token_time = Client.current_time()
+        else:
+            self._token = None
+            self._token_time = 0
+
+        token_req = Request(method="POST", url=self.token_endpoint, json=self.credentials)
+        self.token_refresh_request = self.session.prepare_request(token_req)
+
+    @property
+    def token(self) -> str:
+        if not self.token_valid:
+            response = self.session.send(self.token_refresh_request)
+            self._token = response.json()
+            self._token_time = Client.current_time()
+        return self._token
 
     @property
     def token_endpoint(self) -> str:
         """API endpoint to acquire an API access token."""
-        return f"{self.env_api_url}/oauth/token"
+        return self._version_endpoint("oauth/token")
 
     @property
-    def token(self) -> str:
-        raise NotImplementedError()
+    def token_expiry(self) -> int:
+        """Get the time in seconds since Unix epoch that this Client's token expires."""
+        return self._token_time + self._token.get("expires_in")
+
+    @property
+    def token_valid(self) -> bool:
+        """True if this Client has a token that is unexpired. False otherwise."""
+        return (
+            # a token is required
+            isinstance(self._token, dict)
+            and
+            # check that the current time is less than the token's expiry time
+            # offset by 1 second, to account for a check right near the expiry time
+            Client.current_time() < self.token_expiry - 1
+        )
+
+    def _version_endpoint(self, partial_endpoint: str) -> str:
+        """Get a complete endpoint with base URL and API version."""
+        return f"{self.env_api_url}/api/{self.env_api_version}/{partial_endpoint.lstrip('/')}"
--- tests/api/test_client.py
+++ tests/api/test_client.py
@@ -1,36 +1,88 @@
-from authlib.integrations.requests_client import OAuth2Session
+import time
 
-from littlepay.api.client import Client
+from requests import PreparedRequest, Session
+
+from littlepay.api.client import _current_time, Client
+
+
+def test_current_time():
+    assert _current_time() == int(time.time())
 
 
 def test_Client():
     client = Client("https://example.com", "client_id", "client_secret", "audience")
 
-    assert client.audience == "audience"
+    assert client.credentials == {
+        "audience": "audience",
+        "client_id": "client_id",
+        "client_secret": "client_secret",
+        "grant_type": "client_credentials",
+    }
     assert client.env_api_url == "https://example.com"
-    assert client.client_id == "client_id"
-    assert client.client_secret == "client_secret"
-    assert client.token_endpoint == f"{client.env_api_url}/oauth/token"
+    assert client.env_api_version == "v1"
+    assert client.token_endpoint == f"{client.env_api_url}/api/{client.env_api_version}/oauth/token"
+    assert isinstance(client.token_refresh_request, PreparedRequest)
+    assert client.token_refresh_request.method == "POST"
+    assert client.token_refresh_request.url == client.token_endpoint
 
 
 def test_Client_session():
     client = Client("https://example.com", "client_id", "client_secret", "audience")
 
-    assert isinstance(client.session, OAuth2Session)
-    assert client.session.metadata["audience"] == client.audience
-    assert client.session.client_id == client.client_id
-    assert client.session.client_secret == client.client_secret
-    assert client.session.metadata["grant_type"] == "client_credentials"
-    assert client.session.metadata["token_endpoint"] == client.token_endpoint
+    assert isinstance(client.session, Session)
+    assert ("Accept", "application/json") in client.session.headers.items()
+    assert ("Content-Type", "application/json") in client.session.headers.items()
+    assert client.session.headers["User-Agent"].startswith("cal-itp/littlepay")
 
 
-def test_Client_session_token():
-    client = Client("https://example.com", "client_id", "client_secret", "audience", "token123")
+def test_Client_token(mocker):
+    mocker.patch("littlepay.api.client.Client.current_time", return_value=100)
+    client = Client("https://example.com", "client_id", "client_secret", "audience", token={"expires_in": 300})
 
-    assert client.session.token == "token123"
+    assert client._token == {"expires_in": 300}
+    assert client._token_time == 100
+    assert client.token_valid
 
 
-def test_Client_session_token_None():
-    client = Client("https://example.com", "client_id", "client_secret", "audience", None)
+def test_Client_token_None(mocker):
+    mocker.patch("littlepay.api.client.Client.current_time", return_value=100)
+    client = Client("https://example.com", "client_id", "client_secret", "audience", token=None)
+    client.session = mocker.Mock(
+        send=mocker.Mock(return_value=mocker.Mock(json=mocker.Mock(return_value={"expires_in": 300})))
+    )
 
-    assert client.session.token is None
+    assert client._token is None
+    assert client._token_time == 0
+    assert not client.token_valid
+
+    token = client.token
+
+    assert token == {"expires_in": 300}
+    assert client._token == token
+    assert client._token_time == 100
+    assert client.token_valid
+
+
+def test_Client_version():
+    client = Client("https://example.com", "client_id", "client_secret", "audience", env_api_version="env_ver")
+
+    assert client.env_api_version == "env_ver"
+
+
+def test_Client_version_endpoint():
+    client = Client("https://example.com", "client_id", "client_secret", "audience")
+    partial = "partial/123.json"
+
+    result = client._version_endpoint(partial)
+
+    assert result == f"https://example.com/api/v1/{partial}"
+
+
+def test_Client_version_endpoint_version():
+    version = "env_ver"
+    client = Client("https://example.com", "client_id", "client_secret", "audience", env_api_version=version)
+    partial = "partial/123.json"
+
+    result = client._version_endpoint(partial)
+
+    assert result == f"https://example.com/api/{version}/{partial}"
