d8804d3d32191575235c697cb04e40fc2f46875b
Kegan Maher <kegan@compiler.la>
2023-08-31 00:25:00 +0000

    feat(api/groups): create and remove groups

--- tests/api/test_groups.py
+++ tests/api/test_groups.py
@@ -4,6 +4,11 @@ from littlepay.api.groups import GroupResponse, GroupsMixin
 
 
 @pytest.fixture
+def mock_ClientProtocol_delete(mocker):
+    return mocker.patch("littlepay.api.ClientProtocol._delete", side_effect=lambda *args, **kwargs: True)
+
+
+@pytest.fixture
 def mock_ClientProtocol_get_list_Groups(mocker):
     items = [
         dict(id="0", label="zero", participant_id="zero_0"),
@@ -45,6 +50,17 @@ def test_GroupsMixin_concession_groups_endpoint_group_id_extras(url):
     assert client.concession_groups_endpoint("1234", "extra", "5678") == f"{url}/concession_groups/1234/extra/5678"
 
 
+def test_GroupsMixin_create_concession_group(mock_ClientProtocol_post):
+    client = GroupsMixin()
+
+    result = client.create_concession_group("the-label")
+
+    mock_ClientProtocol_post.assert_called_once_with(client.concession_groups_endpoint(), {"label": "the-label"}, dict)
+    assert isinstance(result, dict)
+    assert result["id"] == "0"
+    assert result["participant_id"] == "zero_0"
+
+
 def test_GroupsMixin_get_concession_groups(mock_ClientProtocol_get_list_Groups):
     client = GroupsMixin()
 
@@ -65,3 +81,12 @@ def test_GroupsMixin_get_concession_groups(mock_ClientProtocol_get_list_Groups):
     assert result_list[2].id == "2"
     assert result_list[2].label == "two"
     assert result_list[2].participant_id == "two_2"
+
+
+def test_GroupsMixin_remove_concession_group(mock_ClientProtocol_delete):
+    client = GroupsMixin()
+
+    result = client.remove_concession_group("1234")
+
+    mock_ClientProtocol_delete.assert_called_once_with(client.concession_groups_endpoint("1234"))
+    assert result is True
